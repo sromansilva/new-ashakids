ef8c7ce8c8b83edbb4be0ac566b2af21ca16150e
AliQueso <p154d0nc10@gmail.com>
2025-07-05 20:34:07 -0500

    se actualizo la version de framentacion thymeleaf

---
+++ backend-spring/src/main/resources/static/js/padre/padres.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.createElement("div");
+  toggleBtn.className = "menu-toggle";
+  toggleBtn.innerHTML = "☰";
+  document.body.appendChild(toggleBtn);
+
+  const sidebar = document.querySelector(".sidebar");
+
+  toggleBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+  });
+});
+
+
+function mostrarModalZoom(link) {
+  const linkBtn = document.getElementById("linkReunion");
+  linkBtn.href = link;
+
+  const bsModal = new bootstrap.Modal(document.getElementById("modalReunion"));
+  bsModal.show();
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  vincularBotonesReservar();
+});

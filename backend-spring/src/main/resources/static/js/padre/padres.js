bb1808ff1eb9e01ac794caffd69bee356f7d06db
AliQueso <p154d0nc10@gmail.com>
2025-07-14 01:25:17 -0500

    se agrego hamburguesa a todos los roles

--- backend-spring/src/main/resources/static/js/padre/padres.js
+++ backend-spring/src/main/resources/static/js/padre/padres.js
@@ -43,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   window.addEventListener("resize", handleResize);
-  handleResize(); // Ejecutar al cargar
+  handleResize();
 });
 
 /*
